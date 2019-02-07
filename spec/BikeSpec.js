159a7c16c0e63e0b8d79b5b841b6372575c4fbad
sg1110 <saule.guzyte@tesco.com>
2019-02-07 10:03:05 +0000

    Working bike added +test

--- spec/BikeSpec.js
+++ spec/BikeSpec.js
@@ -1,1 +1,10 @@
+describe ("Bike", function(){
+  var bike
+  beforeEach(function(){
+    bike = new Bike()
+  })
 
+  it ("should initialize with a working bike", function(){
+    expect(bike.working).toEqual(true)
+  })
+})
